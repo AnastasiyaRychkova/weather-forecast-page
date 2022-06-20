72b9a3613c985393c72053019100bbde507b7060
AnastasiyaRychkova <anast.ry4kova2011@yandex.ru>
2022-06-20 13:54:51 +0300

    feat: Горизонтальная прокрутка

--- src/components/WeeklyForecast.tsx
+++ src/components/WeeklyForecast.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import { DailyForecast } from '../core/types';
+import { useHorizontalScroll } from './utils';
 import WeatherCard from './WeatherCard';
 
 interface IProps {
@@ -11,10 +12,11 @@ const WeeklyForecast: FC<IProps> = ({
 	city,
 	forecast,
 }) => {
+	const ref = useHorizontalScroll<HTMLUListElement>();
 	return forecast && (
 		<section className="weekly-forecast-overview">
 			<h2>{'Погода на неделю в городе '+city}</h2>
-			<ul className="weekly-forecast_week-list">
+			<ul className="weekly-forecast_week-list" ref={ref}>
 			{
 				forecast.map(
 					(weather) => <WeatherCard
---
+++ src/components/utils.ts
@@ -0,0 +1,29 @@
+import { useRef, useEffect } from "react";
+
+export function useHorizontalScroll<T extends HTMLElement>() {
+	const scrolledElemRef = useRef<T>( null );
+
+	useEffect( () => {
+		const scrolledElem = scrolledElemRef.current;
+		if( !scrolledElem )
+			return;
+
+		const onWheel = ( event: WheelEvent ) => {
+			if( event.deltaY === 0 )
+				return;
+			event.preventDefault();
+			scrolledElem.scrollTo({
+				left: scrolledElem.scrollLeft + event.deltaY,
+			});
+		};
+		scrolledElem.addEventListener( "wheel", onWheel );
+
+		return () => {
+			scrolledElem.removeEventListener( "wheel", onWheel );
+		};
+	}, [] );
+
+	return scrolledElemRef;
+}
+
+
