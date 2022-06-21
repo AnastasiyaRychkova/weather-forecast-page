41b62147cc755eaa5df28d1ed7effbac6a78f856
AnastasiyaRychkova <anast.ry4kova2011@yandex.ru>
2022-06-21 15:00:35 +0300

    feat: Текущее время вынесено в отдельный компонент

--- src/components/CityOverview.tsx
+++ src/components/CityOverview.tsx
@@ -1,32 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
-	timeSting,
 	dateString,
 	dayString,
 	weekDayString,
-} from '../core//util';
+} from '../core/util';
+import Time from './Time';
 
 interface IProps {
 	city: string,
 }
 
-const TIME_UPDATE_DELAY = 1000;
-
 const CityOverview = ({
 	city,
 }: IProps) => {
 	const now = new Date();
-	const [time, setTime] = useState( timeSting( now ) );
-	useEffect(() => {
-		const timer = setInterval( () => {
-			setTime( timeSting( new Date() ) );
-		},
-		TIME_UPDATE_DELAY );
-		return () => {
-			clearTimeout( timer );
-		}
-	}, [] );
-
+	
 	return (
 		<div className="city-overview">
 			<h1 className="city">{city}</h1>
@@ -35,11 +23,7 @@ const CityOverview = ({
 				dateTime={dateString( now )}>
 				Сегодня, {weekDayString( now )}, {dayString( now )}
 			</time>
-			<time
-				className="time"
-				dateTime={time}>
-				{time}
-			</time>
+			<Time />
 		</div>
 	);
 };
---
+++ src/components/Time.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { timeSting } from '../core/util';
+
+const TIME_UPDATE_DELAY = 1000;
+
+const Time = () => {
+	const now = new Date();
+	const [time, setTime] = useState( timeSting( now ) );
+	useEffect(() => {
+		const timer = setInterval( () => {
+			setTime( timeSting( new Date() ) );
+		},
+		TIME_UPDATE_DELAY );
+		return () => {
+			clearTimeout( timer );
+		}
+	}, [] );
+	return (
+		<time
+			className="time"
+			dateTime={time}>
+			{time}
+		</time>
+	);
+};
+
+export default Time;
